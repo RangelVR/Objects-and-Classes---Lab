04e80978a69c331ad7c5fa7a8d69f1002b6c0d0f
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-12-03 15:32:28 +0200

    Add files via upload

---
+++ 02. Big Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+
+            for (int i = 2; i <= factorial; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
