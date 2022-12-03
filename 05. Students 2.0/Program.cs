9418b757cc2d16cc3fcfbbce80c6ad43ac53b333
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-12-03 22:30:57 +0200

    Update Program.cs

--- 05. Students 2.0/Program.cs
+++ 05. Students 2.0/Program.cs
@@ -1,80 +1,70 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Students_2._0
+namespace _04.Students
 {
     class Program
     {
         class Student
         {
             public string FirstName { get; set; }
-
             public string LastName { get; set; }
-
-            public int Age { get; set; }
-
+            public string Age { get; set; }
             public string HomeTown { get; set; }
         }
 
-
         static void Main(string[] args)
         {
-            string inputStudent = Console.ReadLine();
-            List<Student> listOfstudents = new List<Student>();
+            List<Student> students = new List<Student>();
+            string[] command = Console.ReadLine().Split().ToArray();
 
-            while (inputStudent != "end")
+            while (command[0] != "end")
             {
-                string[] arr = inputStudent.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-                string firstName = arr[0];
-                string lastName = arr[1];
-                int age = int.Parse(arr[2]);
-                string homeTown = arr[3];
-
-                if (IsStudentExist(listOfstudents, firstName, lastName))
+                string firstName = command[0];
+                string lastName = command[1];
+                string age = command[2];
+                string homeTown = command[3];
+               
+                if (IsStudentExisting(students, firstName, lastName))
                 {
-                    Student student = GetExistingStudent(listOfstudents, firstName, lastName);
-
-                    student.FirstName = firstName;
-                    student.LastName = lastName;
+                    Student student = GetStudent(students, firstName, lastName);
+                    
                     student.Age = age;
                     student.HomeTown = homeTown;
                 }
                 else
                 {
-                    Student student = new Student();
+                    Student newStudent = new Student()
+                    {
+                        FirstName = firstName,
+                        LastName = lastName,
+                        Age = age,
+                        HomeTown = homeTown
+                    };
 
-                    student.FirstName = arr[0];
-                    student.LastName = arr[1];
-                    student.Age = int.Parse(arr[2]);
-                    student.HomeTown = arr[3];
-
-                    listOfstudents.Add(student);
+                    students.Add(newStudent);
                 }
-
-                inputStudent = Console.ReadLine();
-
+                
+                command = Console.ReadLine().Split().ToArray();
             }
 
             string city = Console.ReadLine();
-
-
-            foreach (Student student in listOfstudents)
+            
+            foreach (Student student in students)
             {
                 if (student.HomeTown == city)
                 {
-                    Console.WriteLine($"{student.FirstName} {student.LastName} is" +
-                    $" {student.Age} years old.");
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age } years old.");
                 }
             }
-
+            
         }
 
 
-        static bool IsStudentExist(List<Student> listOfstudents,
-            string firstName, string lastName)
+        static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
         {
-            foreach (var student in listOfstudents)
+            foreach (Student student in students)
             {
                 if (student.FirstName == firstName && student.LastName == lastName)
                 {
@@ -84,17 +74,21 @@ namespace Students_2._0
             return false;
         }
 
-        static Student GetExistingStudent (List<Student> listOfstudents, 
-            string firstName, string lastName)
+
+        static Student GetStudent(List<Student> students, string firstName, string lastName)
         {
-            foreach (Student student in listOfstudents)
+            Student existingStudent = null;
+
+            foreach (Student student in students)
             {
                 if (student.FirstName == firstName && student.LastName == lastName)
                 {
-                    return student;
+                    existingStudent = student;
                 }
             }
-            return null;
+
+            return existingStudent;
         }
     }
+
 }
