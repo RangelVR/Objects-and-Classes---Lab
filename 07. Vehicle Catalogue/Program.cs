9bdbf4ac5a90671227b050180b1d3f06ff497ba2
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-12-04 11:33:44 +0200

    Update Program.cs

--- 07. Vehicle Catalogue/Program.cs
+++ 07. Vehicle Catalogue/Program.cs
@@ -1,110 +1,98 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Vehicle_Catalogue
+namespace _07.Vehicle_Catalogue
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("/").ToArray();
+            Catalog catalog = new Catalog();
+
+            while (input[0] != "end")
+            {
+                string type = input[0];
+                string brand = input[1];
+                string model = input[2];
+                string horsePower = " ";
+                string weight = " ";
+
+                if (type == "Car")
+                {
+                    horsePower = input[3];
+                    Car newCar = new Car(brand, model, horsePower);
+                    catalog.Cars.Add(newCar);
+
+                }
+                else if (type == "Truck")
+                {
+                    weight = input[3];
+                    Truck newTruck = new Truck(brand, model, weight);
+                    catalog.Trucks.Add(newTruck);
+                }
+
+                input = Console.ReadLine().Split("/").ToArray();
+            }
+
+            if (catalog.Cars.Count > 0)
+            {
+                Console.WriteLine("Cars:");
+                foreach (Car car in catalog.Cars.OrderBy(car => car.Brand))
+                {
+                    Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+                }
+            }
+            if (catalog.Trucks.Count > 0)
+            {
+                Console.WriteLine("Trucks:");
+                foreach (Truck truck in catalog.Trucks.OrderBy(truck => truck.Brand))
+                {
+                    Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+                }
+
+            }
+        }
+
+    }
+
     class Car
     {
-        
+        public Car(string brand, string model, string horsePower)
+        {
+            Brand = brand;
+            Model = model;
+            HorsePower = horsePower;
+        }
+
         public string Brand { get; set; }
-
         public string Model { get; set; }
-
         public string HorsePower { get; set; }
     }
 
     class Truck
     {
-        
+        public Truck(string brand, string model, string weight)
+        {
+            Brand = brand;
+            Model = model;
+            Weight = weight;
+        }
         public string Brand { get; set; }
-
         public string Model { get; set; }
-
         public string Weight { get; set; }
     }
 
     class Catalog
     {
-        public Catalog ()
+        public Catalog()
         {
             Cars = new List<Car>();
-
             Trucks = new List<Truck>();
         }
-
         public List<Car> Cars { get; set; }
-
         public List<Truck> Trucks { get; set; }
     }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string input = Console.ReadLine();
-            Catalog catalogType = new Catalog();
-
-            while (input != "end")
-            {
-                string[] arrInput = input.Split("/", StringSplitOptions.RemoveEmptyEntries);
-
-                string type = arrInput[0];
-                string brand = arrInput[1];
-                string model = arrInput[2];
-                string weightHorsePower = arrInput[3];
-
-                if (type == "Car")
-                {
-                    Car newCar = new Car()
-                    {
-                        Brand = brand,
-                        Model = model,
-                        HorsePower = weightHorsePower
-                    };
-                    catalogType.Cars.Add(newCar);
-                }
-                else if (type == "Truck")
-                {
-                    Truck newTruck = new Truck() 
-                    {
-                        Brand = brand,
-                        Model = model,
-                        Weight = weightHorsePower
-                    };
-                    catalogType.Trucks.Add(newTruck);
-                }
-
-                input = Console.ReadLine();
-            }
-
-            catalogType.Cars = catalogType.Cars
-                .OrderBy(x => x.Brand)
-                .ToList();
-
-            catalogType.Trucks = catalogType.Trucks
-                .OrderBy(x => x.Brand)
-                .ToList();
-
-            if (catalogType.Cars.Count > 0)
-            {
-                Console.WriteLine("Cars:");
-
-                foreach (Car car in catalogType.Cars)
-                {
-                    Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
-                }
-            }
-
-            if (catalogType.Trucks.Count > 0)
-            {
-                Console.WriteLine("Trucks:");
-
-                foreach (Truck truck in catalogType.Trucks)
-                {
-                    Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
-                }
-            }
-        }
-    }
 }
