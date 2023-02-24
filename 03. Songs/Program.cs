84773716870f8d05deadd30505926a134f639f96
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-25 00:02:04 +0200

    Update Program.cs

--- 03. Songs/Program.cs
+++ 03. Songs/Program.cs
@@ -1,54 +1,49 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace _03.Songs
+int n = int.Parse(Console.ReadLine());
+
+List<Song> songs = new List<Song>();
+
+while (n > 0)
 {
-    class Song
+    string[] currSong = Console.ReadLine().Split("_").ToArray(); 
+
+    string type = currSong[0];
+    string name = currSong[1];
+
+    Song newSong = new Song
     {
-        public string TypeList { get; set; }
-        public string Name { get; set; }
+        TypeList = type,
+        Name = name,
+    };
+
+    songs.Add(newSong);
+    n--;
+}
+
+string typeList = Console.ReadLine();
+
+if (typeList == "all")
+{
+    foreach (Song song in songs)
+    {
+        Console.WriteLine(song.Name);
     }
-
-    class Program
+}
+else
+{
+    foreach (Song song in songs)
     {
-        static void Main(string[] args)
+        if (song.TypeList == typeList)
         {
-            int nSongs = int.Parse(Console.ReadLine());
+            Console.WriteLine(song.Name);
 
-            List<Song> songs = new List<Song>();
-
-            for (int i = 0; i < nSongs; i++)
-            {
-                string[] data = Console.ReadLine().Split("_");
-
-                Song song = new Song() {
-                    TypeList = data[0],
-                    Name = data[1],
-                };
-                songs.Add(song);
-            }
-
-            string typeList = Console.ReadLine();
-
-            if (typeList == "all")
-            {
-                foreach (Song song in songs)
-                {
-                    Console.WriteLine(song.Name);
-                }
-            }
-            else
-            {
-                foreach (Song song in songs)
-                {
-                    if (song.TypeList == typeList)
-                    {
-                        Console.WriteLine(song.Name);
-                    }
-                }
-            }
         }
     }
+}
 
+class Song
+{
+    public string TypeList { get; set; }
+
+    public string Name { get; set; }
 }
