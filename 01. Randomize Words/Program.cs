a62e8d7e0f4f33949d78ef251c637b3f6be8d050
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-24 23:23:28 +0200

    Update Program.cs

--- 01. Randomize Words/Program.cs
+++ 01. Randomize Words/Program.cs
@@ -1,26 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+string[] words = Console.ReadLine().Split().ToArray() ;
 
-namespace _02.Big_Factorial
+Random random = new Random();
+
+for (int i = 0; i < words.Length; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<string> sentence = Console.ReadLine().Split().ToList();
-
-            Random rnd = new Random();
-
-            for (int i = 0; i < sentence.Count; i++)
-            {
-                int rndIndex = rnd.Next(0, sentence.Count);
-                string exchangeWord = sentence[i];
-                sentence[i] = sentence[rndIndex];
-                sentence[rndIndex] = exchangeWord;
-            }
-            Console.WriteLine(string.Join(Environment.NewLine, sentence));
-        }
-    }
-
+    int randomIndex = random.Next(0, words.Length);
+    string tempWord = words[i];
+    words[i] = words[randomIndex];
+    words[randomIndex] = tempWord;
 }
+
+Console.WriteLine(string.Join(Environment.NewLine, words));
