a3140905b37b8428ebf35ff3df3906f9c78df98b
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-25 11:15:16 +0200

    Update Program.cs

--- 05. Students 2.0/Program.cs
+++ 05. Students 2.0/Program.cs
@@ -1,93 +1,61 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+string studentInfo = Console.ReadLine();
 
-namespace _05.Students_2_0
-    class Program
+List<Student> students = new List<Student>();
+
+while (studentInfo != "end")
+{
+    string[] infoArr = studentInfo.Split();
+
+    string firstName = infoArr[0];
+    string lastName = infoArr[1];
+    int age = int.Parse(infoArr[2]);
+    string homeTown = infoArr[3];
+
+    if (students.Any(x => x.FirstName == firstName && x.LastName == lastName))
     {
-        class Student
+        foreach (var student in students)
         {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Age { get; set; }
-            public string HomeTown { get; set; }
-        }
-
-        static void Main(string[] args)
-        {
-            List<Student> students = new List<Student>();
-            string[] command = Console.ReadLine().Split().ToArray();
-
-            while (command[0] != "end")
+            if (student.FirstName == firstName && student.LastName == lastName)
             {
-                string firstName = command[0];
-                string lastName = command[1];
-                string age = command[2];
-                string homeTown = command[3];
-               
-                if (IsStudentExisting(students, firstName, lastName))
-                {
-                    Student student = GetStudent(students, firstName, lastName);
-                    
-                    student.Age = age;
-                    student.HomeTown = homeTown;
-                }
-                else
-                {
-                    Student newStudent = new Student()
-                    {
-                        FirstName = firstName,
-                        LastName = lastName,
-                        Age = age,
-                        HomeTown = homeTown
-                    };
-
-                    students.Add(newStudent);
-                }
-                
-                command = Console.ReadLine().Split().ToArray();
+                student.Age = age;
+                student.HomeTown = homeTown;
+                break;
             }
-
-            string city = Console.ReadLine();
-            
-            foreach (Student student in students)
-            {
-                if (student.HomeTown == city)
-                {
-                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age } years old.");
-                }
-            }
-            
-        }
-
-
-        static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
-        {
-            foreach (Student student in students)
-            {
-                if (student.FirstName == firstName && student.LastName == lastName)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-
-        static Student GetStudent(List<Student> students, string firstName, string lastName)
-        {
-            Student existingStudent = null;
-
-            foreach (Student student in students)
-            {
-                if (student.FirstName == firstName && student.LastName == lastName)
-                {
-                    existingStudent = student;
-                }
-            }
-
-            return existingStudent;
         }
     }
+    else
+    {
+        Student newStudent = new Student
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Age = age,
+            HomeTown = homeTown
+        };
+        students.Add(newStudent);
+    }
+    
+    studentInfo = Console.ReadLine();
+}
 
+string town = Console.ReadLine();
+
+List<Student> filteredStudents = students.Where(x => x.HomeTown == town).ToList();
+
+foreach (Student student in filteredStudents)
+{
+    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+}
+
+
+
+class Student
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+
+    public string HomeTown { get; set; }
 }
